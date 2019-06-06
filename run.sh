1d0ee9bf0860d498073d59e3ee1831fcc782043f
u2prakash <49615251+u2prakash@users.noreply.github.com>
2019-06-06 01:59:59 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install java-1.8.0-openjdk
-aws s3 ls s3://maven-buckets | tail -1 > bfile
-cat bfile | awk ' { print $4 } ' bfile
-a=bfile
-aws s3 cp s3://maven-buckets/$a $a
-java -jar $a 
+#aws s3 ls s3://maven-buckets | tail -1 > bfile
+#cat bfile | awk ' { print $4 } ' bfile
+#a=bfile
+#aws s3 cp s3://maven-buckets/$a $a
+#java -jar $a 
